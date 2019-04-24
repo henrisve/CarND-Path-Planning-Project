23f6e4e19359954cdb25d22115a8cc88b49f2510
Henrik <henrisve@gmail.com>
2019-04-23 22:41:00 -0400

    added all the things from the qa, and a simple search aalgo, etc etc .. ok, I really need to get better with commit more often =(

--- src/helpers.h
+++ src/helpers.h
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <string>
 #include <vector>
-
+#include <set>
 // for convenience
 using std::string;
 using std::vector;
@@ -28,7 +28,18 @@ string hasData(string s) {
 // Helper functions related to waypoints and converting from XY to Frenet
 //   or vice versa
 //
-
+struct pos{
+  int x;
+  int y;
+};
+struct path{
+  vector<pos> path_list;
+};
+struct best_path_item{
+  path best_path;
+  //vector<pos> path_list;
+  double f;
+};
 // For converting back and forth between radians and degrees.
 constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
@@ -153,5 +164,255 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s,
 
   return {x,y};
 }
+//Todo, maybe move to a helpers.cpp?
+//classes:
 
+/* 
+ *  Car Class, this class will be all the other cars on the road.
+ *  do we need this? or is the 
+ * 
+ * 
+*/
+bool check_xy_blocker(int x, int y, int xmax, int ymax, vector<vector<bool> > map){
+  //Return true if its an ok path
+  return  !(x < 0 || x >= xmax || y < 0 || y >= ymax || map[x][y]);
+}
+
+path search_path(vector<vector<bool> > map,int lane){
+  /*
+  *  This function will calculate the path, based on astar but modified.
+  * 
+  * 
+  */
+ // What do we need. heuristic map,(NO, it can be calulated on fly.)
+ // best_score_map (yes) vector of vector
+ // best_path_map (yes) (maybe these two can be in the same?) 
+ // open_set (yes) a queue of starts to try
+ //
+ struct open_item{
+  double f;
+  double g;
+  int x;
+  int y;
+  bool operator < (const open_item &other) const { return f < other.f; }
+ };
+//bool s::tdoperator<(const open_item& lhs, const open_item& rhs)
+//{
+//      return lhs.f < rhs.f;
+//}; 
+ int max_x = map.size();
+ int max_y = map[0].size();
+
+ vector<vector<best_path_item > > best_path_map(max_x, std::vector<best_path_item>(max_y)); // need to init this
+ std::set<open_item > open_set;
+
+ int y=0;
+ int x=lane;
+ double g=1;
+ double f=999; // no need to calculate
+
+ // move [] = {-1,0,-1};
+ int min_len = 2;
+ int max_len = 10;
+
+ open_item start_position = {f,g,x,y};
+ open_set.insert(start_position);
+ pos start_pos = {x,y};
+ best_path_map[x][y].best_path.path_list.insert(best_path_map[x][y].best_path.path_list.begin(), start_pos);
+ best_path_map[x][y].f = f;
+ int count = 0;
+ vector<vector<float> > map_s(3, std::vector<float>(90));
+ while(!open_set.empty()){
+   count++;
+ //  std::cout << "search it:" << count << std::endl;
+   //for (open_item const& eee : open_set)
+    //{
+    //   std::cout << eee.f << ' ';
+   // }
+   auto current_item = *open_set.begin();
+   open_set.erase(open_set.begin());
+   x=current_item.x;
+   y=current_item.y;
+   g=current_item.g;
+   f=current_item.f;
+   path current_path = best_path_map[x][y].best_path;
+   for(int direction =-1; direction<=1; direction++){
+     
+//##### Why does one item disapear after each round, the best item that is. does it when erase open set also erase in the best map??
+
+
+     bool this_move_ok = true;
+     for(int len=min_len;len <= max_len; len++){
+
+       //now we loop over all the x and y to see we can go here or if its blocked
+       for(int check_y=y;check_y<=y+len;check_y++){
+         //ned to redo:
+         if(direction == 0){
+           this_move_ok = check_xy_blocker(x,check_y,max_x,max_y,map);
+         }else if(direction == -1){
+           this_move_ok = check_xy_blocker(x,check_y,max_x,max_y,map) && check_xy_blocker(x-1,check_y,max_x,max_y,map);
+         }else if(direction == 1){
+           this_move_ok = check_xy_blocker(x,check_y,max_x,max_y,map) && check_xy_blocker(x+1,check_y,max_x,max_y,map);
+         }
+         if(!this_move_ok) break;
+       // # }
+       }
+       if(this_move_ok){
+         
+
+         int landing_y = y + len;
+         int landing_x = x + direction;
+       //  std::cout << "move ok " << x << "," <<  y << " -> " << landing_x << "," << landing_y << std::endl;
+         double new_g = g + len;
+         double new_f = new_g + (max_y - landing_y)*4 + abs(direction)*5*(max_len-len+1);
+        // std::cout << "new is " << new_f << " and old " << best_path_map[landing_x][landing_y].f << std::endl;
+         if(new_f < best_path_map[landing_x][landing_y].f || best_path_map[landing_x][landing_y].f == 0){
+        //   std::cout << "old" << best_path_map[x][y].f << std::endl;
+           best_path_map[landing_x][landing_y].f = new_f;
+           
+           pos this_pos = {landing_x,landing_y};
+           path this_path = current_path;
+           //maybe I need to check if current path list exists?
+           this_path.path_list.insert(this_path.path_list.end() ,this_pos);
+           best_path_map[landing_x][landing_y].best_path = this_path;
+
+           if(landing_y >= max_y-1){
+             std::cout << " found it" << landing_y << " >= " << max_y-1 << std::endl;
+             return this_path;
+           }
+           open_item this_open = {new_f,new_g,landing_x,landing_y};
+          // std::cout << "added new";
+           open_set.insert(this_open);
+         } 
+
+          /*
+          std::cout << "make map";
+          for (auto &i : map_s)
+            std::fill(i.begin(), i.end(), 0);
+          for(int xx=0;xx<3;xx++){
+            for(int yy=0;yy<89;yy++){
+              map_s[xx][yy] = best_path_map[xx][yy].f;   
+            }
+          }
+          map_s[x][y] = -1;
+          map_s[landing_x][landing_y] = 9999;
+          std::vector< std::vector<float> >::const_iterator crow; 
+          std::vector<float>::const_iterator ccol; 
+          std::cout << "## map_s:          #" << std::endl; 
+          for (crow = map_s.begin(); crow != map_s.end(); ++crow){ 
+              for (ccol = crow->begin(); ccol != crow->end(); ++ccol){ 
+                  std::cout << "|";
+                  std::cout << *ccol; 
+              } 
+              std::cout << "##" << std::endl; 
+          } */
+       }else{
+       //  std::cout << "move not ok" << std::endl;
+
+         break;
+       }
+     }
+   }
+ }
+ std::cout << "failed to find any path" << std::endl;
+ path empty_path;
+ return empty_path;
+}
+
+
+
+
+vector<vector<bool> > car_2_map(vector<vector<bool> > map, double car_distance, double rel_speed, float check_car_d, double accelration){
+  /*
+  *This function will return a maps that
+  * will be used in a-star. it takes map as input
+  * input: 
+  * example
+  * t0: 0 0 0  
+  * t1: 0 0 0  
+  * t2: 0 0 0 
+  * t3: 0 0 1  
+  * t4: 0 1 0  
+  *     g g g    
+  * a car that may be on the way of changing lane, should be seen as
+  * a car taking up 2 lanes
+  */
+  // rel_speed: if negative, he drive faster than us, positve, were going to catch up with him
+
+  //if the other car is going away from us in both direction
+  //std::cout << "dist: " << car_distance << ", speed:"  << rel_speed << ", d: " << check_car_d
+  //          << "accel: " << accelration << std::endl;
+  if((rel_speed < 0.001 && car_distance > 0) || (rel_speed > -0.001 && car_distance <0)) return map;
+  int ts;
+  int ta;
+  if(abs(car_distance) < 2){ //abs(rel_speed) < 2 && 
+    ts=2; // if he's next to us
+    ta=-2;
+  //}else if((rel_speed) < 0.001){
+    //almost same spped
+
+  //}else if(rel_speed > 0 && car_distance >= 0)) {
+    //we drive faster, and hes infront of is
+  }else{ //rel_speed < 0 && car_distance <= 0)
+    //we drive slower, and hes behind of is
+    float resultion_factor = 10;
+    ts = round((car_distance/rel_speed)*resultion_factor); //only V, perhaps add a to this!
+    // for the one with a we need to solve the equation 0 = 1/2 A t^2 s + V t -dist
+    accelration = 0;
+    if(abs(accelration) > 0.01){
+      float x1=-1;
+      float x2=-1;
+      float discriminant = rel_speed*rel_speed - 2 * accelration * -car_distance;
+      if (discriminant > 0) {//
+        x1 = (-rel_speed + sqrt(discriminant)) / (accelration);
+        x2 = (-rel_speed - sqrt(discriminant)) / (accelration);
+     //   std::cout << "grr (" << x1 << "|" <<x2<< ")" << std::endl;
+      }else {
+    //      std::cout << "oh noo" << discriminant << std::endl;
+          x1=ts;
+      }
+      if(x1 > 0 && x2 > 0)
+          ta = std::min(x1,x2)*resultion_factor;
+      else if(x1 > 0)
+          ta= x1*resultion_factor;
+      else if(x2> 0)
+          ta= x2*resultion_factor;
+      else
+          ta=ts;
+      ta = std::round(ta);
+    }else{
+   //   std::cout << "acc < min)" << std::endl;
+      ta = ts;
+    }
+  }
+  
+  int lane = floor((check_car_d)/4);
+  
+ // std::cout << "yo (" << ts << "|" << ta << ") : " <<  (check_car_d-2)/4 << " => " << lane << std::endl;
+  for(int t=std::min(ts,ta)-1;t<=std::max(ts,ta)+1;t++){
+    if(t < 90 && t >= 0){
+      // add longer intervall ()
+      map[lane][t]=true; // this point will have a colision if doing nothing.  
+    }
+  }
+  //std::cout << " b" << std::endl;
+  return map;
+}
+int predict_lane_change(){
+  // Go to the classroom, and train the one we have there, then use it here and predict if
+  // a car may change lane. 0=stay, -1= change left, 1= change right, 
+  // then, if this, the car takes up both lanes!
+  return 0;
+}/*
+vector<double> getXYtime(int t,double d,float a,float v, const vector<double> &maps_s, 
+                     const vector<double> &maps_x, const vector<double> &maps_y){
+  //now just go back and solve S = 1/2 A t^2 s + V t
+
+  double s = 
+
+  return getXY(s, d, maps_s, maps_x, maps_y) {
+
+  
+
+}*/
 #endif  // HELPERS_H
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -50,8 +51,17 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  // start in lane 1 (as in qa video)
+  int lane = 1;
+  // reference velocity, less than 50
+  double ref_vel = 0.224; 
+  std::map<int, std::pair<float,float> > prev_car_state;
+  vector<vector<bool> > map(3, std::vector<bool>(90)); //map of everything within 90 timesteps
+  vector<vector<char> > map_s(3, std::vector<char>(90)); //map of everything within 90 meters
+  vector<vector<char> > map_2(3, std::vector<char>(90)); //map of everything within 90 meters
+
+  h.onMessage([&ref_vel, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy,&lane,&map,&map_s,&prev_car_state,&map_2]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -68,7 +78,8 @@ int main() {
         
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+ //         std::cerr << "Faoieuieui" << std::endl;
+    //      std::cout << "eui to port " << std::endl;
           // Main car's localization Data
           double car_x = j[1]["x"];
           double car_y = j[1]["y"];
@@ -90,14 +101,242 @@ int main() {
 
           json msgJson;
 
-          vector<double> next_x_vals;
-          vector<double> next_y_vals;
 
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
 
+          int prev_size = previous_path_x.size();
+
+          if(prev_size > 0){
+            car_s = end_path_s;
+          }
+
+          if(false){
+            ref_vel -= 0.224;
+          }else if(ref_vel < 49.4){
+            ref_vel += 0.244;
+          }
+
+          //Reset our map before each time
+          for (auto &i : map)
+            std::fill(i.begin(), i.end(), false);
+          for (auto &i : map_s)
+            std::fill(i.begin(), i.end(), ' ');
+          for (auto &i : map_2)
+            std::fill(i.begin(), i.end(), ' ');
+          // loop through all other cars
+         // std::cout << "b" << sensor_fusion.size() << std::endl;
+          int cnt=0;
+          // The sensor_fusion have the following content
+          // 0 car's unique ID, 
+          // 1 car's x position in map coordinates, 
+          // 2 car's y position in map coordinates, 
+          // 3 car's x velocity in m/s, 
+          // 4 car's y velocity in m/s,
+          // 5 car's s position in frenet coordinates,
+          // 6 car's d position in frenet coordinates.
+          for(int i=0; i< sensor_fusion.size(); i++){
+            // and if any if these cars is nearby, add them to our map.
+            cnt++;
+            double check_car_s = sensor_fusion[i][5];
+            
+            // what exactly is "in the future"              
+            double vx = sensor_fusion[i][3];
+            double vy = sensor_fusion[i][4];
+            double check_speed = sqrt(vx*vx + vy*vy);
+            double rel_speed = car_speed-check_speed;
+            //check_car_s += ((double)(50-prev_size)*.02*-rel_speed);
+            check_car_s += ((double)prev_size*.02*check_speed);
+            // if within 90 meter in any direction
+            double car_distance = check_car_s-car_s;
+            if(abs(car_distance)<90){
+              int car_id = sensor_fusion[i][0];
+              double accelration;
+              if(prev_car_state.find(car_id) == prev_car_state.end() ){
+                accelration = 0;
+              }else{
+                accelration = rel_speed-prev_car_state[car_id].first;
+                accelration = accelration * 0.2 + prev_car_state[car_id].second * 0.8;
+              }
+              prev_car_state[car_id].first = rel_speed;
+              prev_car_state[car_id].second = accelration;
+              float check_car_d = sensor_fusion[i][6];
+
+              
+             // std::cout << "prui2 " << std::endl;
+              map = car_2_map(map, car_distance, rel_speed, check_car_d, accelration);
+           //   std::cout << "prui3 " << std::endl;
+              int lane = round((check_car_d-2)/4);
+              if((car_distance+10 < 90 && car_distance+10 >= 0) && (lane >=0 && lane <= 2)){
+                // +10 so we also can see behind
+                map_s[lane][car_distance+10] = 'c'; // this point will have a colision if doing nothing.  
+              }
+            }
+          }
+          //here the map is complete: now do the search:
+          path predicted_path = search_path(map,lane);
+          //std::vector<pos>::const_iterator ste; 
+          //for(ste = predicted_path.path_list.begin(); ste != predicted_path.path_list.end(); ++ste){ 
+          //  std::cout << *ste.x; 
+         // }
+          std::cout << "best path: " << predicted_path.path_list.size() << ": " ; 
+          for(int i=0;i<predicted_path.path_list.size(); i++){
+            std::cout << "(" <<  predicted_path.path_list[i].x << ":" << predicted_path.path_list[i].y << ")" ;
+            map_2[predicted_path.path_list[i].x][predicted_path.path_list[i].y] = '0';
+          }
+          for(int xx=0;xx<3;xx++){
+            for(int yy=0;yy<89;yy++){
+              if(map[xx][yy])
+                map_2[xx][yy] = 'C' ;
+            }
+          }          
+          std::cout << std::endl;
+          
+          //print the map!
+          // Note to self: map is where x is time(second), and map_s is where x is s(meter)
+          std::cout << "map has:" << cnt << std::endl;
+          std::vector< std::vector<bool> >::const_iterator row; 
+          std::vector<bool>::const_iterator col; 
+          std::vector< std::vector<char> >::const_iterator crow; 
+          std::vector<char>::const_iterator ccol; 
+          
+
+
+          
+          for (crow = map_2.begin(); crow != map_2.end(); ++crow){ 
+              for (ccol = crow->begin(); ccol != crow->end(); ++ccol){ 
+                  std::cout << "|";
+                  std::cout << *ccol; 
+              } 
+              std::cout << "##" << std::endl; 
+          } 
+          std::cout << "## map_s:          #" << std::endl; 
+          for (crow = map_s.begin(); crow != map_s.end(); ++crow){ 
+              for (ccol = crow->begin(); ccol != crow->end(); ++ccol){ 
+                  std::cout << "|";
+                  std::cout << *ccol; 
+              } 
+              std::cout << "##" << std::endl; 
+          } 
+
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+         // std::cout << "prev size " << prev_size << std::endl;
+          if(prev_size < 2){ //when we start we dont have anypoints here
+            //std::cout << "first"  << std::endl;
+
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }else{
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y-ref_y_prev,ref_x-ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y);
+          }
+
+
+          for(int i=0;i<predicted_path.path_list.size(); i++){
+            predicted_path.path_list[i].x;
+            predicted_path.path_list[i].y;
+
+          }
+         // getXYtime()
+          //as in the qa, we add evenly spaced points every 30 meter.
+          vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          for(int i=0; i< ptsx.size(); i++){
+
+            // shift car reference angle to local car coordinates.
+            double shift_x = ptsx[i]-ref_x;
+            double shift_y = ptsy[i]-ref_y;
+
+            ptsx[i] = (shift_x* cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+            ptsy[i] = (shift_x* sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+          }
+          
+          tk::spline s;
+          //std::cout << "ptsx size"  << ptsx.size() << std::endl;
+          
+          //for (std::vector<double>::const_iterator i = ptsx.begin(); i != ptsx.end(); ++i){
+          //  std::cout << *i << ' ';
+         // }
+            
+
+          //std::cout << "ptsy size"  << ptsy.size() << std::endl;
+          //for (std::vector<double>::const_iterator i = ptsy.begin(); i != ptsy.end(); ++i){
+          //  std::cout << *i << ' ';
+         // }
+      
+          s.set_points(ptsx,ptsy);
+
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+
+          //start with all the prev path pons from last time:
+          for(int i = 0; i < previous_path_x.size(); i++){ // may be  prev_size => previous_path_x.size(); as in qa
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+
+          double x_add_on = 0;
+
+          for(int i = 1; i <= 50 - previous_path_x.size();  i++){
+
+            double N = (target_dist/(.02*ref_vel/2.24));
+            double x_point = x_add_on+(target_x)/N;
+            double y_point = s(x_point);
+            
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            //Rotate back to global coordinates
+            x_point = (x_ref *cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref *sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+            x_point += ref_x; // why not above?
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
+
+          }
+
+        //  std::cout << "end ", next_x_vals.size();
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
